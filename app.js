10e1c9fccb7a5f067d3928e10bec16a42b4fb6dd
jaimesi <si.ja@northeastern.edu>
2023-04-03 17:54:06 -0400

    a9 mongo sp23 update env

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors'
 import mongoose from "mongoose";
+import { config } from 'dotenv';
 import HelloController
     from "./controllers/hello-controller.js"
 import UserController
@@ -8,6 +9,7 @@ import UserController
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 
+config();
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
     || 'mongodb://127.0.0.1:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);
