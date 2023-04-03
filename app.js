feff41bf4d644e7304b3cbfff9177919957616c5
jaimesi <si.ja@northeastern.edu>
2023-04-03 18:06:17 -0400

    a9 mongo sp23 debugging

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import TuitsController
 
 config();
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb://127.0.0.1:27017/tuiter'
+    || 'mongodb://127.0.0.1:27017/tuiter';
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
